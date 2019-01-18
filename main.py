4b0770893aa0d0ae1d384e4a9d842be7234a84d9
atagiev <andrej-tagiev@yandex.ru>
2019-01-19 01:26:58 +0300

    Убрана многопоточность Проверка ИГ выставлена раз в 2 минуты чтобы лучше обрабатывать входящие сообщения в ТГ

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import telepot
 import config
 import sqlite3
 import feedparser
-from threading import Thread
+#from threading import Thread
 
 AllOk=True
+time_OLD=time.time()
 f=open("message_id.txt","r")
 m_id_old=int(f.read())
 bot=telepot.Bot(config.TOKEN)
@@ -22,43 +23,42 @@ except:
     pass
 
 #обработка входящих сообщений с тг и добавление ников в бд
-def database_work():
+def Telegram_Work():
     global cursor,conn,bot,m_id_old,AllOk
-    while(AllOk):
-        try:
-            upd=bot.getUpdates(-1)
-            m_id=upd[0]["message"]["message_id"]
-            c_id=upd[0]["message"]["chat"]["id"]
-            if (m_id>m_id_old):
-                try:
-                    text=upd[0]["message"]["text"]
-                    if ((text=="/stopBot") and (c_id in config.admin_id)):
-                        AllOk=False;
-                        bot.sendMessage(c_id,config.stopBot)
-                    elif (text=="/help"):
-                        bot.sendMessage(c_id,config.help)
-                    elif (text=="/start"):
-                        bot.sendMessage(c_id,config.start)
-                    else:
-                        if ((text[0:3]=="add") or(text[0:3]=="Add")):
-                            cursor.execute("INSERT INTO subs VALUES(?,?)",(c_id,text[4:],))
-                        if ((text[0:3]=="del") or (text[0:3]=="Del")):
-                            cursor.execute("DELETE FROM subs WHERE (tgid= ?) AND (igname= ?)",(c_id,text[4:],))
-                        conn.commit()
-                        cursor.execute("SELECT igname FROM subs WHERE tgid = ?",(c_id,))
-                        substring="Вы подписаны на \n"
-                        for i in cursor.fetchall():
-                            substring=substring+i[0]+"\n"
-                        bot.sendMessage(c_id,substring)
-                        substring=""
-                except:
-                    pass
-                m_id_old=m_id
-        except:
-            pass
+    try:
+        upd=bot.getUpdates(-1)
+        m_id=upd[0]["message"]["message_id"]
+        c_id=upd[0]["message"]["chat"]["id"]
+        if (m_id>m_id_old):
+            try:
+                text=upd[0]["message"]["text"]
+                if ((text=="/stopBot") and (c_id in config.admin_id)):
+                    AllOk=False;
+                    bot.sendMessage(c_id,config.stopBot)
+                elif (text=="/help"):
+                    bot.sendMessage(c_id,config.help)
+                elif (text=="/start"):
+                    bot.sendMessage(c_id,config.start)
+                else:
+                    if ((text[0:3]=="add") or(text[0:3]=="Add")):
+                        cursor.execute("INSERT INTO subs VALUES(?,?)",(c_id,text[4:],))
+                    if ((text[0:3]=="del") or (text[0:3]=="Del")):
+                        cursor.execute("DELETE FROM subs WHERE (tgid= ?) AND (igname= ?)",(c_id,text[4:],))
+                    conn.commit()
+                    cursor.execute("SELECT igname FROM subs WHERE tgid = ?",(c_id,))
+                    substring="Вы подписаны на \n"
+                    for i in cursor.fetchall():
+                        substring=substring+i[0]+"\n"
+                    bot.sendMessage(c_id,substring)
+                    substring=""
+            except:
+                pass
+            m_id_old=m_id
+    except:
+        pass
 
 #парсинг rss ленты с https://websta.me/rss/n/username
-def parseIGposts(igname,postid,posttext):
+def parse_IG_posts(igname,postid,posttext):
     workinglink="https://websta.me/rss/n/"+igname
     myfeed=feedparser.parse(workinglink)
     s=myfeed.entries[0]["link"]
@@ -67,42 +67,38 @@ def parseIGposts(igname,postid,posttext):
     posttext=s[:s.find("<a href=https://")]
 
 #работа с новыми постами в ig
-def ig_posts():
+def ig_posts(j):
+        parse_IG_posts(j,postid,posttext)
+        cursor.execute("SELECT postid FROM posts WHERE igname = ?",(j,))
+        if (postid <> cursor.fetchone()[0]):
+            cursor.execute("DELETE FROM posts WHERE igname = ?",(j,))
+            cursor.execute("INSERT INTO posts VALUES(?,?)",(j,postid,))
+            conn.commit()
+            msgtext=j+" posted new [photo](https://instagram.com/p/"+postid+")"+" with comment:\n"+"_"+posttext+"_"
+            cursor.execute("SELECT tgid FROM subs WHERE igname=? ",(j,))
+            for i in cursor.fetchall():
+                bot.sendMessage(i[0],msgtext, Markdown)
+
+def Instagram_Work():
     global conn,cursor,bot,AllOk
     allIGnicks=set()
-    while (AllOk):
-        allIGnicks.clear()
-        cursor.execute("SELECT igname FROM subs")
-        for j in cursor.fetchall():
-            allIGnicks.add(j[0])
-        for j in allIGnicks:
-            parseIGposts(j,postid,posttext)
-            cursor.execute("SELECT postid FROM posts WHERE igname = ?",(j,))
-            if (postid <> cursor.fetchone()[0]):
-                cursor.execute("DELETE FROM posts WHERE igname = ?",(j,))
-                cursor.execute("INSERT INTO posts VALUES(?,?)",(j,postid,))
-                conn.commit()
-                msgtext=j+" posted new [photo](https://instagram.com/p/"+postid+")"+" with comment:\n"+"_"+posttext+"_"
-                cursor.execute("SELECT tgid FROM subs WHERE igname=? ",(j,))
-                for i in cursor.fetchall():
-                    bot.sendMessage(i[0],msgtext, Markdown)
+    allIGnicks.clear()
+    cursor.execute("SELECT igname FROM subs")
+    for j in cursor.fetchall():
+        allIGnicks.add(j[0])
+    for j in allIGnicks:
+        ig_posts(j)
+        ig_stories(j)
 
-#обработка историй из иг
-def ig_stories():
-    global conn,cursor,bot,AllOk
-    allIGnicks=set()
-    while (AllOk):
-        allIGnicks.clear()
-        cursor.execute("SELECT igname FROM subs")
-        for j in cursor.fetchall():
-            allIGnicks.add(j[0])
-        
+#main
+while AllOk:
+    Telegram_Work()
+    if ((time.time()-time_OLD)>120):
+        time_OLD=time.time()
+        Instagram_Work()
 
-
-Thread(target = database_work).start()
-time.sleep(1)
-Thread(target = ig_posts).start()
-Thread(target = ig_stories).start()
+#Thread(target = Telegram_Work).start()
+#Thread(target = Instagram_Work).start()
 f=open("message_id.txt","w")
 f.write(str(m_id_old))
 cursor.close()
