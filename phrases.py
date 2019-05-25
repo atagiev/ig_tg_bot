0d58cac7e109bb5d8257937b28272a0ebd75aa9e
atagiev <andrej-tagiev@yandex.ru>
2019-05-25 15:18:35 +0300

    phrases

---
+++ phrases.py
@@ -0,0 +1,3 @@
+help="Send 'add username' to subscribe to Instagram user\nSend 'del username' to unsubscribe from Instagram user"
+start="Welkome\n Send 'add username' to subscribe to Instagram user\nSend 'del username' to unsubscribe from Instagram user"
+stopBot="You killed the Bot"
